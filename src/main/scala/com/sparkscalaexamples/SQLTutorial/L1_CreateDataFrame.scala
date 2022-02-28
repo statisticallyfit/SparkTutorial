25729d4f19b6506438fa54dbc1eb50fc744f788a
statisticallyfit <statisticallyfit@gmail.com>
2022-03-01 00:57:02 +0200

    add puny functions

--- src/main/scala/com/sparkscalaexamples/SQLTutorial/L1_CreateDataFrame.scala
+++ src/main/scala/com/sparkscalaexamples/SQLTutorial/L1_CreateDataFrame.scala
@@ -3,7 +3,7 @@ package com.sparkscalaexamples.SQLTutorial
 
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{SparkSession, DataFrame}
-
+import org.apache.spark.sql.types.{DataType, StringType, IntegerType, BooleanType, DoubleType, StructField, StructType}
 
 /**
  * WEB PAGE SOURCE = https://sparkbyexamples.com/spark/different-ways-to-create-a-spark-dataframe/
@@ -162,6 +162,73 @@ object ConvertRDDToDataFrame {
 
 		List(df_noheader, df, df_delim, df_inferSchema)
 	}
+
+	// TODO read with quotes / nullvalues / dateformat = https://hyp.is/Gpl27Jh2Eey9h_sXVK2vZA/sparkbyexamples.com/spark/spark-read-csv-file-into-dataframe/
+
+
+	/**
+	 * Use if you know the schema of the file ahead of time and do not want to use the `inferSchema` option
+	 * for column names and types. Can use a user-defined custom schema.
+	 *
+	 * @param spark
+	 * @param filepath
+	 * @return
+	 */
+	//import org.apache.spark.sql.types.AtomicType
+
+	// Pass in the schema types (stringtype, integertype, ... in order of how they should correspond to column
+	// names, then pair those up with teh column names to make the structtype manually here (using fold)
+
+	def usingReadFileByCSVWithCustomSchema(spark: SparkSession,
+								    schemaNameTypePairs: Seq[(String, DataType)],
+								    /*schema: StructType,*/
+								    filepath: String): DataFrame = {
+
+		val emptyStruct: StructType = new StructType()
+
+		val userSchema: StructType = schemaNameTypePairs.foldLeft(emptyStruct){
+			case (accStruct, (name, tpe)) => accStruct.add(name = name, dataType = tpe, nullable = true)
+		}
+
+		val dfWithSchema: DataFrame = spark.read.format("csv")
+			.option("header", "true")
+			.schema(userSchema)
+			.load(filepath)
+
+		dfWithSchema.printSchema()
+		dfWithSchema.show()
+
+		dfWithSchema
+	}
+
+
+	def usingReadTXTFile(spark: SparkSession, filepath: String): DataFrame = {
+
+		val df = spark.read.text(filepath)
+		df.printSchema()
+		df.show()
+		df
+	}
+
+	def usingReadJSONFile(spark: SparkSession, filepath: String): DataFrame = {
+
+		val df = spark.read.json(filepath)
+		df.printSchema()
+		df.show()
+		df
+	}
+
+	// TODO more xml detail here = https://sparkbyexamples.com/spark/spark-read-write-xml/
+	def usingReadXMLFile(spark: SparkSession, filepath: String): DataFrame = {
+		val df = spark.read
+			.format("com.databricks.spark.xml")
+			.option(key = "rowTag", value = "person")
+			.xml(filepath)
+
+		df.printSchema()
+		df.show()
+		df
+	}
 }
 
 
@@ -192,8 +259,10 @@ object L1_CreateDataFrame extends App {
 	val (df4_, df4) = ConvertRDDToDataFrame.usingSeqToDF(spark, data, columnNames)
 	val df5 = ConvertRDDToDataFrame.usingCreateDataFrameFromSparkSessionViaSequence(spark, data, columnNames)
 	val df6 = ConvertRDDToDataFrame.usingCreateFataFrameFromListOfRowsAndSchema(spark, data, columnNames)
+
+
 	val List(df7_, df7, df7_delim, df7_inferSchema) = ConvertRDDToDataFrame.usingReadFileByCSV(spark, filepath =
-		"src/main/resources/zipcodes.csv")
+		ZIPCODES_FILE_CSV)
 	//TODO left off here to check what inferschema does
 
 	assert(List(df1, df2, df3, df4, df5, df6).combinations(2)
@@ -208,13 +277,18 @@ object L1_CreateDataFrame extends App {
 
 
 	// CSV part -----------------------------------------------------------------------------------------------------------
-	import org.apache.spark.sql.types.{StringType, StructField, StructType}
+	//import org.apache.spark.sql.types.{StringType, IntegerType, BooleanType, DoubleType, StructField, StructType}
 
-	val DF7_COLNAMES = List("RecordNumber", "Zipcode", "ZipCodeType", "City", "State", "LocationType", "Lat", "Long",
+	val ZIPCODES_FILE_CSV = "src/main/resources/zipcodes.csv"
+	val FILE_TXT = "src/main/resources/file.txt"
+	val FILE_JSON = "src/main/resources/file.json"
+
+	val ZIPCODES_COLNAMES = List("RecordNumber", "Zipcode", "ZipCodeType", "City", "State", "LocationType", "Lat", "Long",
 		"Xaxis", "Yaxis", "Zaxis", "WorldRegion", "Country","LocationText", "Location", "Decommisioned", "TaxReturnsFiled",
 		"EstimatedPopulation", "TotalWages", "Notes")
 
-	assert(df7.columns.toList == DF7_COLNAMES &&
+
+	assert(df7.columns.toList == ZIPCODES_COLNAMES &&
 		df7.columns.toList == df7_delim.columns.toList &&
 		df7.columns.toList == df7.schema.fields.map(structField => structField.name).toList,
 
@@ -223,5 +297,17 @@ object L1_CreateDataFrame extends App {
 
 	assert(df7.schema.fields.map(structfield => structfield.dataType).forall(_ == StringType))
 	assert(df7.schema == df7_delim.schema)
+
+
+
+	// Reading CSV Files with user-specified custom schema
+	val schemaTypes: Seq[DataType] = List(IntegerType, IntegerType, StringType, StringType, StringType, StringType,
+		DoubleType, DoubleType, IntegerType, DoubleType, DoubleType, StringType, StringType, StringType, StringType,
+		BooleanType, StringType, IntegerType, IntegerType, StringType)
+	val schemaNameTypePairs: Seq[(String, DataType)] = ZIPCODES_COLNAMES.zip(schemaTypes)
+
+	val df8: DataFrame = ConvertRDDToDataFrame.usingReadFileByCSVWithCustomSchema(spark, schemaNameTypePairs, ZIPCODES_FILE_CSV)
+	val df9: DataFrame = ConvertRDDToDataFrame.usingReadTXTFile(spark, ZIPCODES_TXT_FILE)
+
 
 }
